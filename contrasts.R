8c0ae2ce2364dca5b0fb55470b9f2f9acd1bd420
kylehumphrey <kylehumphrey9@gmail.com>
2018-02-28 07:30:44 -0600

    Correct mean contrasts for GCSWLI, change to differences from baseline between groups

--- contrasts.R
+++ contrasts.R
@@ -3,8 +3,8 @@ linfct_means <- rbind(
   'Week 12_WLC' = c(1, 0, 1, 0, 0, 0),
   # 'Week 24_WLC' = c(1, 0, 0, 1, 0, 0),
   'Baseline_GSCWLI' = c(1, 1, 0, 0, 0, 0),
-  'Week 12_GSCWLI' = c(1, 1, 1, 0, 0, 0),
-  'Week 24_GSCWLI' = c(1, 1, 0, 1, 0, 0)
+  'Week 12_GSCWLI' = c(1, 1, 1, 0, 1, 0),
+  'Week 24_GSCWLI' = c(1, 1, 0, 1, 0, 1)
 )
 
 linfct_diffs_base <- rbind(
@@ -18,6 +18,6 @@ linfct_diffs_grps <- rbind(
   # GSCWLI - WLC
   # "GCSWLI" after underscore in name to print on the GCSWLI line in the table
   'Baseline_GSCWLI' = c(0, 1, 0, 0, 0, 0),
-  'Week 12_GSCWLI' = c(0, 1, 0, 0, 1, 0) #,
+  'Week 12_GSCWLI' = c(0, 0, 0, 0, 1, 0) #,
   # 'Week 24_GSCWLI - WLC' = c(0, 1, 0, 0, 0, 1)
 )
