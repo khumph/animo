12833db55f69e932dd7332e03974ff06e3f3f5d1
kylehumphrey <kylehumphrey9@gmail.com>
2018-02-28 07:35:43 -0600

    Change pvals to difference to be more informative, change to 2 sig figs for printed pvalues

--- table-functions.R
+++ table-functions.R
@@ -5,7 +5,9 @@ get_glhts <- function(linfct, models_list, labels_df) {
   ) %>% set_names(names(labels_df))
 }
 
-get_results_df <- function(glht_obj, variable_name, pvals = T) {
+get_results_df <- function(glht_obj, variable_name, difference = T,
+                           sigfigs = 3, digits = NULL,
+                           log_transformed = str_detect(variable_name, 'log')) {
   # returns a formatted data frame with 95% confidence intervals and p-values
   #   resulting from the contrasts given in a multcomp::glht object
   # inputs: glht_obj, a multcomp::glht object with contrasts of interest
@@ -14,36 +16,36 @@ get_results_df <- function(glht_obj, variable_name, pvals = T) {
   ci_df <- confint(glht_obj, calpha = qnorm(.975)) %>%
     broom::tidy() %>% select(-rhs)
 
-  if (str_detect(variable_name, 'log')) {
-    if (str_detect(variable_name, '\\+ 1')) {
-      if (pvals == T) {
-        ci_df <- ci_df %>% mutate_at(vars(-lhs), funs((exp(.) - 1) * 100))
-      } else {
-        ci_df <- ci_df %>% mutate_at(vars(-lhs), funs((exp(.) - 1)))
-      }
-    } else if (pvals == T) {
+  if (log_transformed) {
+    if (difference) {
       ci_df <- ci_df %>% mutate_at(vars(-lhs), funs((exp(.) - 1) * 100))
+    } else if (str_detect(variable_name, '\\+ 1')) {
+      ci_df <- ci_df %>% mutate_at(vars(-lhs), funs((exp(.) - 1)))
     } else {
       ci_df <- ci_df %>% mutate_at(vars(-lhs), funs(exp))
     }
   }
 
-  if (pvals == T) {
-    pval_df <- summary(object = glht_obj, test = adjusted(type = "none")) %>%
+  if (difference) {
+    pval_df <-
+      summary(object = glht_obj, test = adjusted(type = "none")) %>%
       broom::tidy() %>%
       select(lhs, p.value)
 
     results_df <- full_join(ci_df, pval_df, by = 'lhs')
+  } else {
+    results_df <- ci_df
+  }
 
+  if (difference & log_transformed) {
     results_df <- results_df %>%
       mutate_at(vars(estimate, conf.low, conf.high),
-                funs(sprintf("%1.f", .)))
+                funs(sprintf("%.1f", .)))
   } else {
-    results_df <- ci_df %>%
+    results_df <- results_df %>%
+      rowwise() %>%
       mutate_at(vars(estimate, conf.low, conf.high),
-                funs(format(signif(., 3),
-                            scientific = F,
-                            trim = T, drop0trailing = T)))
+                funs(format(., digits = sigfigs, scientific = F, trim = T)))
   }
 
   results_df <- results_df %>%
@@ -51,11 +53,11 @@ get_results_df <- function(glht_obj, variable_name, pvals = T) {
                              " (", conf.low, ", ", conf.high, ")")) %>%
     select(-starts_with('conf'))
 
-  if (pvals == T) {
+  if (difference) {
     results_df <- results_df %>%
-      mutate(p.value = ifelse(p.value < 0.001,
-                              "p < 0.001",
-                              sprintf("p = %#.3f", p.value)),
+      mutate(p.value = ifelse(p.value < 0.01,
+                              "p < 0.01",
+                              sprintf("p = %#.2g", p.value)),
              estimate = paste0(estimate, ", ", p.value)) %>%
       select(-p.value)
   }
@@ -68,7 +70,7 @@ get_results_df <- function(glht_obj, variable_name, pvals = T) {
   return(results_df)
 }
 
-get_results <- function(glht_list, pvals = T, labels_df) {
+get_results <- function(glht_list, difference = T, labels_df) {
   map2_df(glht_list, names(labels_df),
-          ~ get_results_df(.x, .y, pvals))
+          ~ get_results_df(.x, .y, difference))
 }
