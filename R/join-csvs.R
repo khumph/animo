cab8761af2976854cc639c6671a6e38f0063f55a
KyleHumphrey <kylehumphrey9@gmail.com>
2018-04-05 11:10:03 -0700

    Use dplyr to autodetect common variables

--- R/join-csvs.R
+++ R/join-csvs.R
@@ -5,11 +5,8 @@ main <- function() {
   args <- commandArgs(trailingOnly = T)
   input_files <- head(args, -1)
 
-  dfs <- map(input_files, ~ read.csv(.x, stringsAsFactors = F))
-
-  variables_shared <- map(dfs, names) %>% reduce(intersect)
-
-  reduce(dfs, full_join, by = variables_shared) %>%
+  map(input_files, ~ read.csv(.x, stringsAsFactors = F)) %>%
+    reduce(full_join) %>%
     write.csv(row.names = F)
 }
 
