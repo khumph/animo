d4f8d0ac219a397cbfa94112a5dc4cc26b0d70dc
KyleHumphrey <kylehumphrey9@gmail.com>
2018-04-05 08:41:55 -0700

    Update render script to autodetect output dir

--- R/render.R
+++ R/render.R
@@ -1,9 +1,27 @@
+#' Render an Rmd document
+#'
+#' Automatically determine output directory and output format based on a
+#' specified path
+#'
+#' Currently only inplemented to knit word or html documents
+#'
+#' @param args command line arguments (1) path to input and filename (2) path to
+#'  output file and filename
 main <- function() {
   args <- commandArgs(trailingOnly = T)
   input_file <- args[1]
-  output_file <- args[2]
-  output_dir <- args[3]
-  output_extension <- strsplit(output_file, "\\.")[[1]][-1]
+  output_path <- args[2]
+
+  output_path_separated <- strsplit(output_path, "\\/")[[1]]
+  if (length(output_path_separated) > 1) {
+    output_file <- tail(output_path_separated, 1)
+    output_dir <- paste0(head(output_path_separated, -1), collapse = "/")
+  } else {
+    output_file <- output_path
+    output_dir <- NULL
+  }
+
+  output_extension <- tail(strsplit(output_file, "\\.")[[1]], 1)
 
   rmarkdown::render(input = input_file,
                     output_format = ifelse(output_extension == "docx",
