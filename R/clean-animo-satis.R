a466780ec23f66e818e58c32fd2a1e7c56fb6b01
KyleHumphrey <kylehumphrey9@gmail.com>
2018-04-11 10:45:36 -0700

    Convert overall satisfaction rating to numeric

--- R/clean-animo-satis.R
+++ R/clean-animo-satis.R
@@ -1,6 +1,6 @@
 clean_satisfaction <- function(df) {
   df %>%
-    mutate_at(vars(ends_with("tss"), -contains("why")),
+    mutate_at(vars(ends_with("tss"), -unsatisfied_why_tss, -no_rec_why_tss),
               funs(parse_number)) %>%
     ungroup()
 }
